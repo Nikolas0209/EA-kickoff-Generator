a8e56cd2975190f359875ce50f7d8116850e8218
Nikolas Žilić <zilicnikolas@gmail.com>
2026-04-12 17:03:27 +0200

    Add random international kickoff generator endpoint

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,14 +4,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, { tlsInsecure: true });
+const client = new MongoClient(uri);
+let db = null;
 
 export async function connectDB(){
   try{
+    if(db){
+      return db
+    }
+
     await client.connect();
-    console.log('Connect to the MangooDB')
-    return client.db('EAkickoffGenerator')
+    console.log('Connect to the MangooDB');
+
+    db = client.db('EAkickoffGenerator');
+    return db;
   } catch(error){
-    console.log(error)
+    console.log(error);
   }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,36 @@
 import express from "express";
+import { connectDB } from './db.js';
 
-//const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from express');
-})
+app.get('/countries', async (req, res) => {
+  try{
+    const db = await connectDB();
+    const collection = db.collection('countries');
+
+    const teams = await collection.find().toArray();
+
+    const homeTeamIndex = Math.floor(Math.random() * teams.length);
+    const homeTeam = teams[homeTeamIndex];
+
+    let awayTeamIndex = Math.floor(Math.random() * teams.length);
+
+    while(awayTeamIndex === homeTeamIndex){
+      awayTeamIndex = Math.floor(Math.random() * teams.length);
+    };
+
+    const awayTeam = teams[awayTeamIndex];
+
+    const kickOffTeams = {
+      homeTeam,
+      awayTeam
+    }
+
+    res.status(200).json(kickOffTeams);
+  } catch(error){
+    res.status(500).json({error: 'The kick-off could not be generated.'});
+  }
+});
 
 app.get('/about', (req, res) => {
   res.send('This is the route page')
