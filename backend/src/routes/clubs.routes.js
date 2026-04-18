aabec74dcdfb3f187b89cded4bc87eeecef46583
Nikolas Žilić <zilicnikolas@gmail.com>
2026-04-18 13:43:05 +0200

    add club rating-based kickoff endpoint

--- backend/src/routes/clubs.routes.js
+++ backend/src/routes/clubs.routes.js
@@ -24,7 +24,44 @@ router.get('/', async (req, res) => {
 
     res.status(200).json(kickOffTeams);
   }catch(error){
-   res.status(500).json({error: 'The kick-off could not be generated.'})
+   res.status(500).json({error: 'The kick-off could not be generated.'});
+  }
+});
+
+router.get('/club-ratings', async (req, res) => {
+  try{
+    const teams = await getCollection('clubs');
+
+    if(teams.length < 2){
+      return res.status(400).json({error: 'Not enough teams'});
+    };
+
+    const homeTeam = getRandomTeam(teams);
+
+    const getAllRatings = teams.map(team => team.stars);
+    const lowestStarsTeam = Math.min(...getAllRatings); 
+
+    const maxRating = homeTeam.stars + 1;
+    const minRating = Math.max(homeTeam.stars - 1, lowestStarsTeam);
+
+    const teamRating = teams.filter(team => team.stars >= minRating && team.stars <= maxRating);
+
+    let availableAwayTeams = teamRating.filter(team => !team._id.equals(homeTeam._id));
+
+    if(availableAwayTeams.length === 0){
+      availableAwayTeams = teams.filter(team => !team._id.equals(homeTeam._id));
+    }
+
+    const awayTeam = availableAwayTeams[Math.floor(Math.random() * availableAwayTeams.length)];
+
+    const kickOffTeams = {
+      homeTeam,
+      awayTeam
+    }
+
+    res.status(200).json(kickOffTeams);
+  }catch(error){
+    res.status(500).json({error: 'The kick-off could not be generated'});
   }
 });
 
